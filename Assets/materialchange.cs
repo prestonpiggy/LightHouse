47aec7d9ebf3405253ee8888f7ad30f947ce61e3
Ville Leino <villee.leino@gmail.com>
2019-04-26 15:18:05 +0300

    upload

---
+++ Assets/materialchange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class materialchange : MonoBehaviour {
+    Material mat;
+    // Use this for initialization
+    void Start()
+    {
+        mat = FindObjectOfType<MeshRenderer>().material;
+        gameObject.GetComponentInChildren<MeshRenderer>().material = mat;
+
+        int numOfChildren = transform.childCount;
+        for (int i = 0; i < numOfChildren; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            child.GetComponent<MeshRenderer>().material = mat;
+        }
+    }
+
+        // Update is called once per frame
+        void Update () {
+		
+	    }
+}
